23f16e87dee34d5b54693655842a0aeed813e06b
Trivium <trivium@Triviums-MacBook-Pro.local>
2021-04-22 09:38:12 -0400

    app update

--- app/.ipynb_checkpoints/NYC-st-app-checkpoint.py
+++ app/.ipynb_checkpoints/NYC-st-app-checkpoint.py
@@ -5,34 +5,37 @@ import pickle
 # - pickle final working model
 
 # app title
-st.title('NYC EMS Call Volume')
+st.title('NYC EMS Call Volume Estimator')
 
 # About Section
 about = ''
 st.write(f'{about}')
 
 # Model Section
-st.write('Predict the EMS Call Volume in NYC:')
+st.write('''Can we predict emergency call volume in NYC using time, weather, and traffic data? While these features are beyond our control, the goal of this project is to increase preparedness for a surge in emergency services given specific conditions. Using predictive modeling, we hope to be able to provide an indication of when the city should increase staff in emergency call centers or even on response teams.
+
+Experiment the paramaters on the left to understand how features impact call volume.''')
+
 
 # sliders for each model param:
+st.sidebar.write('Use the sliders to predict the EMS Call Volume in NYC.')
+hour = st.sidebar.slider('Hour',0,24)
 
-hour = st.slider('Hour',0,24)
+temp = st.sidebar.slider('Temp (F)',0,105)
 
-temp = st.slider('Temp (F)',0,105)
+incidences = st.sidebar.slider('Incidences',0,53) #get actual range for this
 
-incidences = st.slider('Incidences',0,53) #get actual range for this
+day = st.sidebar.slider('Day',1,31)
 
-day = st.slider('Day',1,31)
-
-month = st.slider('Month',1,12)
+month = st.sidebar.slider('Month',1,12)
 
 year = 2017
 
-prcp = st.slider('Percipitation',1,6)
+prcp = st.sidebar.slider('Percipitation',1,6)
 
-snwd = st.slider('Snow Depth (inches)',1,24)
+snwd = st.sidebar.slider('Snow Depth (inches)',1,24)
 
-snow = st.slider('Snow (inches)',1,36)
+snow = st.sidebar.slider('Snow (inches)',1,36)
 
 params = [[hour, temp, incidences, day, month, year, prcp, snwd, snow]]
 
