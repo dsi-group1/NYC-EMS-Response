bf76dac43f7ef41150f69063380934484ba5c7e0
Trivium <trivium@Triviums-MacBook-Pro.local>
2021-04-24 11:45:56 -0400

    app update

--- app/NYC-st-app.py
+++ app/NYC-st-app.py
@@ -1,43 +1,55 @@
 import streamlit as st
 import pickle
 
-# TO DO:
-# - pickle final working model
-
 # app title
-st.title('NYC EMS Call Volume Estimator')
+st.title('NYC Emergency Call Volume Estimator')
 
 # About Section
-about = ''
-st.write(f'{about}')
-
-# Model Section
 st.write('''Can we predict emergency call volume in NYC using time, weather, and traffic data? While these features are beyond our control, the goal of this project is to increase preparedness for a surge in emergency services given specific conditions. Using predictive modeling, we hope to be able to provide an indication of when the city should increase staff in emergency call centers or even on response teams.
+___
+### Data Dictionary:
 
-Experiment the paramaters on the left to understand how features impact call volume.''')
+| Feature                   | Notes                                         |
+| ---                       | ---                                           |
+| Hour                      | Hour of the day (0-23)                        |
+| Temp (F)                  | Temperature in Fahrenheit                     |
+| Incidences                | Number of traffic incidents within the hour   |
+| Day                       | Day of the month (1-31)                       |
+| Month                     | Month of the year (1-12)                      |
+| Precipitation             | Precipitation by intensity (1-3 : L,M,H)      |
+| Snow Depth                | Snow depth on ground in inches                |
+___
+### Datasets:
+
+[EMS](https://data.cityofnewyork.us/Public-Safety/EMS-Incident-Dispatch-Data/76xm-jjuj) | [Weather](https://www.ncdc.noaa.gov/cdo-web/search) | [Traffic](https://data.ny.gov/Transportation/511-NY-Events-Beginning-2010/ah74-pg4w)
+
+### Contributors:
+[Chris Caress](https://www.linkedin.com/in/chris-caress-4245a51b5/) | [Christina Holland](https://www.linkedin.com/in/christina-holland-7400a1140/) | [Ashley Poon](https://www.linkedin.com/in/ashley-poon-y95/) | [David Romo](https://www.linkedin.com/in/daromo/)
+
+''')
 
 
 # sliders for each model param:
-st.sidebar.write('Use the sliders to predict the EMS Call Volume in NYC.')
-hour = st.sidebar.slider('Hour',0,24)
+st.sidebar.write('## Use the sliders to predict the EMS Call Volume in NYC.')
+hour = st.sidebar.slider('Hour',0,23)
 
 temp = st.sidebar.slider('Temp (F)',0,105)
 
-incidences = st.sidebar.slider('Incidences',0,53) #get actual range for this
+incidents = st.sidebar.slider('Traffic Incidents',0,53) #get actual range for this
 
 day = st.sidebar.slider('Day',1,31)
 
 month = st.sidebar.slider('Month',1,12)
 
-year = 2017
+year = 2016
 
-prcp = st.sidebar.slider('Percipitation',1,6)
+prcp = st.sidebar.slider('Precipitation',0,2)
 
-snwd = st.sidebar.slider('Snow Depth (inches)',1,24)
+snow = 16
 
-snow = st.sidebar.slider('Snow (inches)',1,36)
+snwd = st.sidebar.slider('Snow Depth (inches)',0,24)
 
-params = [[hour, temp, incidences, day, month, year, prcp, snwd, snow]]
+params = [[hour, temp, incidents, day, month, year, prcp, snwd, snow]]
 
 # import pickled model
 model = pickle.load(open('../modeling/app-model.pkl', 'rb'))
@@ -46,13 +58,13 @@ model = pickle.load(open('../modeling/app-model.pkl', 'rb'))
 prediction = model.predict(params)
 
 if prediction > 0 and prediction < 105:
-    level = 'LOW'
+    level = '''**LOW**'''
 
 if prediction > 106 and prediction < 326:
-    level = 'AVERAGE'
+    level = '''**AVERAGE**'''
     
 if prediction > 326:
-    level = 'HIGH'
+    level = '''**HIGH**'''
     
-st.write(f'Call volume is predicted to be: {int(prediction)}')
-st.write(f'This is {level} for NYC.')
+st.sidebar.write(f'Call volume is predicted to be: **{int(prediction)}**')
+st.sidebar.write(f'This is {level} for NYC.')
