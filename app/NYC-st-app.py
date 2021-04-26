d5d559d94ee693c98e09dd00f16aecc02dd2b6ee
Trivium <trivium@triviums-mbp.myfiosgateway.com>
2021-04-26 10:49:56 -0400

    app update

--- app/NYC-st-app.py
+++ app/NYC-st-app.py
@@ -72,7 +72,7 @@ params = [[hour, TAVG_CALC, Traffic_Incidents, day, month, PRCP, year, SNWD, AWN
            SNOW, TMAX, TMIN ]] # hard-coded params
 
 # import  model
-model = tf.keras.models.load_model('./resources/app-nn/')
+model = tf.keras.models.load_model('/app-nn/')
 
 # sc = StandardScaler()
 
